2058871fe7713e7361325a7825701dcdc264a8c1
Estudio <jfsuarezb@icloud.com>
2020-08-03 18:52:48 -0500

    Implemented List Component

--- client/index.js
+++ client/index.js
@@ -2,7 +2,9 @@ Vue.component('date-picker', {
 
     data: function() {
         return {
-            date: ""
+            date: "",
+            buttonState:false,
+            usersData: []
         }
     },
 
@@ -11,23 +13,53 @@ Vue.component('date-picker', {
         <div id=\"datepicker\">
             <h1 id=\"datepickerbanner\">Selecciona una Fecha: </h1>
             <input type="date" v-model="date"></input>
-            <v-btn small id="datepickerbutton" v-on:click="clickHandler">Buscar</v-btn>
+            <v-btn v-bind:disabled="buttonState" small id="datepickerbutton" v-on:click="clickHandler">Buscar</v-btn>
+            <v-list two-line subheader id="userlist">
+                <v-list-item class="listClass" v-for="user in usersData" v-bind:key="user.uid">
+                    <v-list-item-content>
+                        <v-list-item-title v-text="'Nombre: ' + user.name"></v-list-item-title>
+                        <v-list-item-subtitle v-text="'Edad: ' + user.age"></v-list-item-subtitle>
+                    </v-list-item-content>
+                    <v-list-item-action>
+                        <v-btn v-on:click="userClickHandler(user.uid)" text small id="viewButton">Ver</v-btn>
+                    </v-list-item-action>
+                </v-list-item>
+            </v-list>
         </div>
 
     `,
 
     methods:{
 
-        clickHandler: async function() {
+        clickHandler: function() {
+
+            this.getBuyers().then(function(data) {
+                app.$children[1].usersData = data["getBuyers"]
+            }).catch(function(error) {
+                alert(error)
+            })
+
+        },
+
+        getBuyers: async function() {
             let unixt = Date.parse(this.date)/1000
             try {
-                await fetch(`http://localhost:3000/sync?date=${unixt}`)
-                let resp = await fetch(`http://localhost:3000/buyers`)
-                app.usersData = resp.json()
-                console.log(app.usersData)
-            } catch(e) {
-                alert(e)
+                this.buttonState = true
+                await fetch(window.location.href + `sync?date=${unixt}`)
+                let resp = await fetch(window.location.href + "buyers")
+                let data = await resp.json()
+                this.buttonState = false
+                return data
+            } catch (error) {
+                alert(error)
+                this.buttonState = false
             }
+        },
+
+        userClickHandler: function(uid) {
+
+            console.log(uid)
+
         }
 
     }
@@ -37,10 +69,6 @@ Vue.component('date-picker', {
 var app = new Vue({
 
     el: "#app",
-    data: {
-        usersData: {},
-        userData:{}
-    },
     vuetify: new Vuetify(),
 
 })
